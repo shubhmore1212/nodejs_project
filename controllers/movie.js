b367adac5b0910cd0709f4d5da70fe02b550348d
Shubham More <shubham.more@joshsoftware.com>
2023-07-12 11:09:50 +0530

    validated request body of movie search api

--- controllers/movie.js
+++ controllers/movie.js
@@ -1,10 +1,26 @@
 import { response } from "../config/responseUtil.js";
 import { tryCatch } from "../middlewares/tryCatch.js";
+import {
+  checkForError,
+  movieSearchSchema,
+} from "../middlewares/validateParams.js";
 
 import { insertMovie, searchMovie } from "../services/movie.js";
 
 export const getMovie = tryCatch(async (req, res) => {
   const { movie_name } = req.body;
+
+  const { error } = movieSearchSchema.validate(
+    {
+      movie_name,
+    },
+    {
+      abortEarly: false,
+    }
+  );
+
+  checkForError(error);
+
   const result = await searchMovie(movie_name);
   return response(res, 200, result);
 });
